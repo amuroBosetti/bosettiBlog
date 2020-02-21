7b26f85c978acd582e959b6b70b749516bb3d41b
amuroBosetti <amuro.bosetti@gmail.com>
2020-02-21 12:28:04 -0300

    :lipstick: changed RSS feed theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,7 +61,7 @@ module.exports = {
         short_name: `GatsbyJS`,
         start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `#663399`,
+        theme_color: `#7e2699`,
         display: `minimal-ui`,
         icon: `content/assets/gatsby-icon.png`,
       },
